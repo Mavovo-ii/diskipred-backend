766200f1bd7f0defd33a5eaa485eff4da0b273f4
Thokozani Mkhize <mavovo.mkhize@yahoo.com>
2025-07-10 16:29:57 +0200

    Fix import paths in server js to relative routes

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ import express from "express";
 import dotenv from 'dotenv'
 import cors from 'cors' 
 import connectDB from "./config/db.js";
-import fixtureRoutes from "../backend/routes/fixtureRoutes.js"
-import userRoutes from "../backend/routes/userRoutes.js"
-import predictionRoutes from "../backend/routes/predictionRoutes.js"
-import prizeRoutes from "../backend/routes/prizeRoutes.js"
-import leaderboardRoutes from "../backend/routes/leaderBoardRoutes.js"
+import fixtureRoutes from "./routes/fixtureRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
+import predictionRoutes from "./routes/predictionRoutes.js"
+import prizeRoutes from "./routes/prizeRoutes.js"
+import leaderboardRoutes from "./routes/leaderBoardRoutes.js"
 import { scoreEndedMatches } from "./utils/scoreEndedMatches.js";
-import teamRoutes from "../backend/routes/teams.js"
-import auth from "../backend/routes/auth.js"
+import teamRoutes from "./routes/teams.js"
+import auth from "./routes/auth.js"
 import protectedRoutes from "./routes/protected.js"
 
 
